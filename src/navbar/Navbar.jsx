2d17441601a95435f4fdd1a770803cb3bf328745
ahnafi <devsulthon@gmail.com>
2023-06-11 12:08:46 +0700

    add

---
+++ src/navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+const Navbar = () => {
+  let Links = [
+    { name: "HOME", link: "/" },
+    { name: "SERVICE", link: "/" },
+    { name: "ABOUT", link: "/" },
+    { name: "BLOG'S", link: "/" },
+    { name: "CONTACT", link: "/" },
+  ];
+  const [open, setOpen] = useState(false);
+  return (
+    <header className="shadow-md  w-full fixed top-0 left-0">
+      <nav className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
+        <div
+          className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
+      text-gray-800"
+        >
+          BRAND
+        </div>
+        <button
+          onClick={() => setOpen(!open)}
+          className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
+        >
+          <span className="w-[25px] h-[4px] my-1 block bg-black rounded-full"></span>
+          <span className="w-[25px] h-[4px] my-1 block bg-black rounded-full"></span>
+          <span className="w-[25px] h-[4px] my-1 block bg-black rounded-full"></span>
+        </button>
+        <ul
+          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in 
+          ${open ? "top-20 " : "top-[-490px]"}`}
+        >
+          {Links.map((link) => (
+            <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7 ">
+              <a
+                href={link.link}
+                className="text-gray-800 hover:text-gray-400 duration-500"
+              >
+                {link.name}
+              </a>
+            </li>
+          ))}
+          {/* <button
+            className="bg-indigo-600 text-white font-[Poppins] py-2 px-6 rounded md:ml-8 hover:bg-indigo-400 
+    duration-500" >
+            get started
+          </button> */}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+export default Navbar;
